bcffdcc0ea5a835fba2639c961fc0d2350c81b37
Michael Peterson <MadMathMike@gmail.com>
2023-12-06 22:13:38 -0700

    Pass day 5 part 2

--- 2023/day-05/src/main.rs
+++ 2023/day-05/src/main.rs
@@ -1,11 +1,49 @@
 fn main() {
     let part1_input = include_str!("./part1.txt");
-    let result = part1(part1_input);
-
+    let (seeds, maps) = parse_input(part1_input);
+    
+    let result = part1(&seeds, &maps);
     dbg!(result);
+
+    use std::time::Instant;
+    let now = Instant::now();
+
+    let result = part2(&seeds, &maps);
+    dbg!(result);
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
 
-fn part1(input: &str) -> i64 {
+fn part2(seeds: &Vec<i64>, maps: &[Vec<RangeMap>; 7]) -> i64 {
+    let mut seeds_iter = seeds.iter();
+    let mut smallest_location = i64::MAX;
+    let mut seed_candidate = seeds_iter.next();
+    while seed_candidate != None {
+        let seed_start = seed_candidate.unwrap();
+        let range = seeds_iter.next().unwrap();
+        let stop = seed_start + range;
+
+        for seed in *seed_start .. stop {
+            let location = traverse_maps(maps, seed);
+            if location < smallest_location {
+                smallest_location = location;
+            }
+        }
+
+        seed_candidate = seeds_iter.next();
+    }
+
+    smallest_location
+}
+
+fn part1(seeds: &Vec<i64>, maps: &[Vec<RangeMap>; 7]) -> i64 {
+    seeds.iter()
+        .map(|seed| traverse_maps(maps, *seed))
+        .min().unwrap()
+}
+
+fn parse_input(input: &str) -> (Vec<i64>, [Vec<RangeMap>; 7]) {
     let mut lines = input.lines().filter(|l| !l.is_empty());
 
     let seeds = lines
@@ -100,33 +138,67 @@ fn part1(input: &str) -> i64 {
     }
 
     let maps = [
-        &seed_to_soil,
-        &soil_to_fertilizer,
-        &fertilizer_to_water,
-        &water_to_light,
-        &light_to_temperature,
-        &temperature_to_humidity,
-        &humidity_to_location        
+        seed_to_soil,
+        soil_to_fertilizer,
+        fertilizer_to_water,
+        water_to_light,
+        light_to_temperature,
+        temperature_to_humidity,
+        humidity_to_location
     ];
 
-    seeds.map(|seed| traverse_maps(&maps, seed))
-    .min().unwrap()
+    (seeds.collect(), maps.map(collapse_maps))
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone, Copy)]
 struct RangeMap {
     start_inclusive: i64,
     end_exclusive: i64,
     addend: i64
 }
 
-fn parse_range_map (input: &str) -> RangeMap {
+fn parse_range_map(input: &str) -> RangeMap {
     let mut input_parts = input.split(' ');
     let target_start = input_parts.next().unwrap().parse::<i64>().unwrap();
     let source_start = input_parts.next().unwrap().parse::<i64>().unwrap();
     let range = input_parts.next().unwrap().parse::<i64>().unwrap();
 
     RangeMap { start_inclusive: source_start, end_exclusive: source_start + range, addend: target_start - source_start }
+}
+
+fn collapse_maps(mut range_maps: Vec<RangeMap>) -> Vec<RangeMap> {
+    range_maps.sort_by(|a, b| a.start_inclusive.cmp(&b.start_inclusive));
+
+    range_maps
+        .iter()
+        .fold(Vec::<RangeMap>::new(), |mut acc, range_map|{
+            if acc.len() == 0 {
+                acc.push(*range_map);
+
+                return acc;
+            }
+
+            let previous = acc.last().unwrap();
+
+            if previous.end_exclusive == range_map.start_inclusive && previous.addend == range_map.addend {
+                // pop the old one off the list
+                let previous = acc.remove(acc.len() - 1);
+
+                // collapse the two
+                let collapsed = collapse(&previous, range_map);
+
+                // add the new collapsed one back on
+                acc.push(collapsed);
+            } else {
+                acc.push(*range_map);
+            }
+
+            acc
+        })
+}
+
+fn collapse(first: &RangeMap, second: &RangeMap) -> RangeMap {
+    RangeMap { start_inclusive: first.start_inclusive, end_exclusive: second.end_exclusive, addend: first.addend }
 }
 
 fn find_addend(range_maps: &Vec<RangeMap>, input: i64) -> i64 {
@@ -139,7 +211,7 @@ fn find_addend(range_maps: &Vec<RangeMap>, input: i64) -> i64 {
     return 0;
 }
 
-fn traverse_maps(maps_array: &[&Vec<RangeMap>], start: i64) -> i64 {
+fn traverse_maps(maps_array: &[Vec<RangeMap>], start: i64) -> i64 {
     let mut return_val = start;
     for maps in maps_array.iter() {
         let addend = find_addend(&maps, return_val);
@@ -179,6 +251,19 @@ mod part1_tests {
     }
 
     #[test]
+    fn correctly_collapses_maps() {
+        let mut soil_to_fertilizer = Vec::<RangeMap>::new();
+        soil_to_fertilizer.push(parse_range_map("0 15 37"));
+        soil_to_fertilizer.push(parse_range_map("37 52 2"));
+        soil_to_fertilizer.push(parse_range_map("39 0 15"));
+
+        let result = collapse_maps(soil_to_fertilizer);
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0], RangeMap {start_inclusive: 0, end_exclusive: 15, addend: 39});
+        assert_eq!(result[1], RangeMap {start_inclusive: 15, end_exclusive: 54, addend: -15});
+    }
+
+    #[test]
     fn traverses_maps() {
         let mut seed_to_soil = Vec::<RangeMap>::new();
         seed_to_soil.push(parse_range_map("50 98 2"));
@@ -196,9 +281,9 @@ mod part1_tests {
         fertilizer_to_water.push(parse_range_map("57 7 4"));
 
         let maps_array = [
-            &seed_to_soil,
-            &soil_to_fertilizer,
-            &fertilizer_to_water
+            seed_to_soil,
+            soil_to_fertilizer,
+            fertilizer_to_water
         ];
 
         let destination = traverse_maps(&maps_array, 79);
@@ -251,7 +336,50 @@ humidity-to-location map:
 60 56 37
 56 93 4";
 
-        let result = part1(input);
+        let (seeds, maps) = parse_input(input);
+        let result = part1(&seeds, &maps);
         assert_eq!(result, 35);
+    }
+
+    #[test]
+    fn part2_finds_lowest_location() {
+        let input = 
+"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+        let (seeds, maps) = parse_input(input);
+        let result = part2(&seeds, &maps);
+        assert_eq!(result, 46);
     }
 }
