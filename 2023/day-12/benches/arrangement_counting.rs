bb7fafb8db9a5358a659d816e21b7e54a9662c0a
Michael Peterson <madmathmike@gmail.com>
2023-12-15 08:19:02 -0700

    Add another benchmark

--- 2023/day-12/benches/arrangement_counting.rs
+++ 2023/day-12/benches/arrangement_counting.rs
@@ -4,11 +4,20 @@ use day_12::count_valid_arrangements;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function(
-        "Counting arrangements", 
+        "Counting arrangements 1", 
         |b| b.iter(|| 
             count_valid_arrangements(
                 black_box("????.######..#####.?????.######..#####.?????.######..#####.?????.######..#####.?????.######..#####."),
                 black_box(&vec![1,6,5,1,6,5,1,6,5,1,6,5,1,6,5])
+        ))
+    );
+
+    c.bench_function(
+        "Counting arrangements 2", 
+        |b| b.iter(|| 
+            count_valid_arrangements(
+                black_box("?###??????????###??????????###??????????###??????????###????????"),
+                black_box(&vec![3,2,1,3,2,1,3,2,1,3,2,1,3,2,1])
         ))
     );
 }
