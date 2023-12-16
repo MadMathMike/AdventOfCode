a739be4a11abd3e3e79fb2660bb882b26353beff
Michael Peterson <madmathmike@gmail.com>
2023-12-15 18:14:45 -0700

    Remove final parameter from recursive function in preparation for memoization

--- 2023/day-12/src/lib.rs
+++ 2023/day-12/src/lib.rs
@@ -1,82 +1,64 @@
 use std::iter::repeat;
 
-pub fn count_valid_arrangements(mask: &str, damaged_segment_lengths: &[usize]) -> usize {
-    // println!("Mask: {}", mask);
-    // let start = std::time::Instant::now();
-
-    let num_of_segment_gaps = damaged_segment_lengths.len() - 1;
-    let num_of_damaged_springs:usize = damaged_segment_lengths.iter().sum();
-    let num_of_unassigned_working_springs = 
-        mask.len() 
-        - num_of_segment_gaps // Each segment gap must have at least one working spring
-        - num_of_damaged_springs;
-
-    let count = count_valid_arrangements_recursive(
-        mask, 
-        num_of_unassigned_working_springs,
-        &damaged_segment_lengths
-    );
-
-    // let duration = start.elapsed();
-    // println!("Time elapsed is: {:?}", duration);
-
-    count
-}
-
-// TODO: MAY need to turn remaining_damaged_segments back into &[usize]
-fn count_valid_arrangements_recursive(
-    remaining_mask: &str,
-    num_of_unassigned_working_springs: usize,
-    remaining_damaged_segments: &[usize]) 
-    -> usize 
+pub fn count_valid_arrangements(mask: &str, damaged_segments: &[usize]) -> usize 
 {
-    if remaining_damaged_segments.len() == 0 {
-        let mut arrangement_chunk = String::with_capacity(remaining_mask.len());
+    if damaged_segments.len() == 0 {
+        let mut arrangement_chunk = String::with_capacity(mask.len());
         repeat('.')
-            .take(num_of_unassigned_working_springs)
+            .take(mask.len())
             .for_each(|f| arrangement_chunk.push(f));
 
-        return if matches(remaining_mask, &arrangement_chunk) {
+        return if matches(mask, &arrangement_chunk) {
             1
         } else {
             0
         }
     }
 
+    // For a given mask length, "??????????????".len() == 14,
+    // and a given damaged segment list, [1,1,3], and knowing
+    // that at least 1 working spring must go between each
+    // damaged section, how many "free" or "available" working
+    // springs do we have? We will try placing various amounts
+    // of working springs before the next damaged segment, but
+    // only up to this limit.
+    let free_working_springs:usize = // 14 - 2 - 7 = 7 available working springs
+        mask.len() // From example: 14
+        - (damaged_segments.len() - 1) // From example: 3 - 1 = 2
+        - damaged_segments.iter().sum::<usize>(); // From example: 1 + 1 + 3 = 5
+
     let mut valid_arrangement_count = 0;
-    for i in 0..(num_of_unassigned_working_springs + 1) {
-        let mut arrangement_chunk = String::with_capacity(remaining_mask.len());
+    for i in 0..=free_working_springs {
+        let mut arrangement_chunk = String::with_capacity(mask.len());
         repeat('.')
             .take(i)
             .for_each(|f| arrangement_chunk.push(f));
 
-        if remaining_damaged_segments.len() > 0 { 
+        if damaged_segments.len() > 0 { 
             repeat('#')
-                .take(remaining_damaged_segments[0])
+                .take(damaged_segments[0])
                 .for_each(|f| arrangement_chunk.push(f));
 
-            if remaining_damaged_segments.len() > 1 {
+            if damaged_segments.len() > 1 {
                 arrangement_chunk.push('.');
             }
         }
 
         valid_arrangement_count += 
-            if matches(remaining_mask, &arrangement_chunk) {
-                count_valid_arrangements_recursive(
-                    &remaining_mask[arrangement_chunk.len()..],
-                    num_of_unassigned_working_springs - i, 
-                    &remaining_damaged_segments[1..]
+            if matches(mask, &arrangement_chunk) {
+                count_valid_arrangements(
+                    &mask[arrangement_chunk.len()..],
+                    &damaged_segments[1..]
                 )
             } else {
                 0
             }
     }
-
     valid_arrangement_count
 }
 
 fn matches(mask: &str, arrangement: &str) -> bool {
-    let mask_chars = &mask[0..arrangement.len()];
+    let mask_chars = &mask[..arrangement.len()];
 
     arrangement.char_indices()
         .all(|(i, char)| 
